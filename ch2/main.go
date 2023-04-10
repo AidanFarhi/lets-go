295557b399708dae801e122debe52560b772e12c
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-04-09 21:32:20 -0400

    finished chapter 2

--- ch2/main.go
+++ ch2/main.go
@@ -1,6 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+const (
+	idKey   = "id"
+	nameKey = "name"
+)
 
 func main() {
 
@@ -31,4 +39,45 @@ func main() {
 	fmt.Println(floatVal / 0) // +Inf
 	floatVal = 0
 	fmt.Println(floatVal / 0) // NaN
+
+	// complex numbers
+	complexVal1 := complex(2.5, 3.1)
+	complexVal2 := complex(10.2, 2)
+	fmt.Println(complexVal1 + complexVal2)
+	fmt.Println(complexVal1 - complexVal2)
+	fmt.Println(complexVal1 * complexVal2)
+	fmt.Println(complexVal1 / complexVal2)
+	fmt.Println(real(complexVal1))
+	fmt.Println(imag(complexVal1))
+	fmt.Println(cmplx.Abs(complexVal1))
+
+	// type conversions
+	var type1 int = 10
+	var type2 float64 = 30.2
+	var type3 float64 = float64(type1) + type2
+	var type4 int = type1 + int(type2)
+	fmt.Println(type3, type4)
+
+	// var vs. :=
+	var myVal = 10
+	myVal2 := 10
+	fmt.Println(myVal, myVal2)
+
+	numVal, stringVal := 100, "hello"
+	fmt.Println(numVal, stringVal)
+
+	// using const
+	const myConst = "yolo"
+	fmt.Println(myConst)
+	newId := idKey + "123"
+	fmt.Println(newId)
+	nameKey := "newNameKey"
+	fmt.Println(nameKey)
+
+	// untyped constant
+	const untypedConstant = 10
+	var val1 int = untypedConstant
+	var val2 float64 = untypedConstant
+	var val3 byte = untypedConstant
+	fmt.Println(val1, val2, val3)
 }
