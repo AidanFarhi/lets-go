c71a238cbb9dd6eb1e7032817b1b1e7985102a93
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-04-02 14:06:24 -0400

    finished ch6/ex3

--- ch6/ex3/main.go
+++ ch6/ex3/main.go
@@ -1,23 +1,16 @@
 package main
 
-import "fmt"
-
-func UpdateSlice(strSlice []string, str string) {
-	strSlice[len(strSlice)-1] = str
-	fmt.Println("In UpdateSlice: ", strSlice)
-}
-
-func GrowSlice(strSlice []string, str string) {
-	strSlice = append(strSlice, str)
-	fmt.Println("In GrowSlice: ", strSlice)
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
 }
 
 func main() {
-	strings := []string{"one", "two", "three"}
-	fmt.Println("Before UpdateSlice: ", strings)
-	UpdateSlice(strings, "four")
-	fmt.Println("After UpdateSlice: ", strings)
-	fmt.Println("Before GrowSlice: ", strings)
-	GrowSlice(strings, "five")
-	fmt.Println("After GrowSlice: ", strings)
+	persons := make([]Person, 0, 100_000_000) // this runs much faster
+	// persons := []Person{}
+	n := 100_000_000
+	for i := 0; i < n; i++ {
+		persons = append(persons, Person{"Bob", "Smith", 34})
+	}
 }
