b80fc855bb6a70d2c8ff5de3574c4e9cd1c37049
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-04-26 23:15:21 -0400

    finished ch7 ex2

--- ch7/ex2/main.go
+++ ch7/ex2/main.go
@@ -1,13 +1,38 @@
 package main
 
 import (
-	"fmt"
+	"errors"
+	"io"
+	"os"
 	"sort"
+	"strconv"
 )
+
+type Ranker interface {
+	Ranking() []string
+}
+
+func RankPrinter(r Ranker, w io.Writer) {
+	for i, r := range r.Ranking() {
+		io.WriteString(w, strconv.Itoa(i+1)+" "+r+"\n")
+	}
+}
+
+type MatchResult struct {
+	t1 string
+	s1 int
+	t2 string
+	s2 int
+}
 
 type Team struct {
 	Name    string
 	Players []string
+}
+
+type Pair struct {
+	Key string
+	Val int
 }
 
 type League struct {
@@ -15,87 +40,67 @@ type League struct {
 	Wins  map[string]int
 }
 
-func (l League) MatchResult(t1, t2 string, s1, s2 int) {
-	if s1 > s2 {
-		w, ok := l.Wins[t1]
-		if ok {
-			l.Wins[t1] = w + 1
-		} else {
-			l.Wins[t1] = 1
-		}
-	} else if s2 > s1 {
-		w, ok := l.Wins[t2]
-		if ok {
-			l.Wins[t2] = w + 1
-		} else {
-			l.Wins[t2] = 1
-		}
+func (l League) Ranking() []string {
+	winners := []string{}
+	pairs := []Pair{}
+	for k, v := range l.Wins {
+		pairs = append(pairs, Pair{Key: k, Val: v})
 	}
+	sort.Slice(pairs, func(i, j int) bool { return pairs[i].Val > pairs[j].Val })
+	for _, p := range pairs {
+		winners = append(winners, p.Key)
+	}
+	return winners
 }
 
-func (l League) Ranking() []string {
-	result := make([]string, 0, 0)
-	scoreMap := make(map[int][]string)
-	scores := make([]int, 0, 0)
-	for k, v := range l.Wins {
-		teamNames, ok := scoreMap[v]
-		if ok {
-			containsTeam := false
-			for _, t := range teamNames {
-				if t == k {
-					containsTeam = true
-					break
-				}
-			}
-			if !containsTeam {
-				teamNames = append(teamNames, k)
-			}
-		} else {
-			scoreMap[v] = []string{k}
-		}
-		scores = append(scores, v)
-	}
-	sort.Sort(sort.Reverse(sort.IntSlice(scores)))
-	for _, s := range scores {
-		teams := scoreMap[s]
-		for _, t := range teams {
-			r := fmt.Sprintf("Team[%s]Wins[%d]", t, s)
-			result = append(result, r)
+func (l League) TeamExists(team string) bool {
+	found := false
+	for _, t := range l.Teams {
+		if t.Name == team {
+			found = true
+			break
 		}
 	}
-	return result
+	return found
+}
+
+func (l *League) AddMatchResult(mr MatchResult) error {
+	if !l.TeamExists(mr.t1) {
+		return errors.New(mr.t1 + " does not exist")
+	}
+	if !l.TeamExists(mr.t2) {
+		return errors.New(mr.t2 + " does not exist")
+	}
+	if mr.s1 > mr.s2 {
+		l.Wins[mr.t1]++
+	}
+	if mr.s2 > mr.s1 {
+		l.Wins[mr.t2]++
+	}
+	return nil
 }
 
 func main() {
-	realMadrid := Team{
-		Name:    "Real Madrid",
-		Players: []string{"Modric", "Carvajal"},
+
+	teamNames := []string{"Liverpool", "Chelsea", "Newcastle", "Brentford"}
+	playerNames := []string{"Haaland", "Iwobi", "Aguero", "Pedro"}
+	matchResults := []MatchResult{
+		MatchResult{t1: "Liverpool", s1: 3, t2: "Chelsea", s2: 2},
+		MatchResult{t1: "Liverpool", s1: 5, t2: "Newcastle", s2: 1},
+		MatchResult{t1: "Brentford", s1: 2, t2: "Liverpool", s2: 0},
+		MatchResult{t1: "Newcastle", s1: 1, t2: "Chelsea", s2: 1},
 	}
-	barcelona := Team{
-		Name:    "Barcelona",
-		Players: []string{"Gundhagen", "Pedri"},
-	}
-	laLiga := League{
-		Teams: []Team{realMadrid, barcelona},
+	league := League{
+		Teams: []Team{},
 		Wins:  make(map[string]int),
 	}
-	laLiga.MatchResult("Real Zarazoga", "Barcelona", 3, 2)
-	laLiga.MatchResult("Real Madrid", "Barcelona", 3, 1)
-	laLiga.MatchResult("Benfica", "Barcelona", 1, 3)
-	laLiga.MatchResult("Real Madrid", "Barcelona", 2, 5)
-	laLiga.MatchResult("Real Madrid", "Getafe", 1, 2)
-	laLiga.MatchResult("Real Madrid", "Barcelona", 3, 2)
-	laLiga.MatchResult("Getafe", "Barcelona", 3, 1)
-	laLiga.MatchResult("Real Madrid", "Barcelona", 1, 3)
-	laLiga.MatchResult("Real Madrid", "Atletico Madrid", 2, 5)
-	laLiga.MatchResult("Real Betis", "Barcelona", 1, 2)
-	laLiga.MatchResult("Real Madrid", "Barcelona", 3, 2)
-	laLiga.MatchResult("Getafe", "Barcelona", 3, 1)
-	laLiga.MatchResult("Real Madrid", "Mallorca", 1, 3)
-	laLiga.MatchResult("Real Sociodad", "Barcelona", 2, 5)
-	laLiga.MatchResult("Real Madrid", "Barcelona", 1, 2)
-	rankings := laLiga.Ranking()
-	for _, r := range rankings {
-		fmt.Println(r)
+
+	for _, t := range teamNames {
+		league.Teams = append(league.Teams, Team{Name: t, Players: playerNames})
+		league.Wins[t] = 0
 	}
+	for _, mr := range matchResults {
+		league.AddMatchResult(mr)
+	}
+	RankPrinter(league, os.Stdout)
 }
