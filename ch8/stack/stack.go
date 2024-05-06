3ba8bc610834163fb5caf4993c9727db4cb3a2e2
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-05-05 21:31:33 -0400

    added generic functions

--- ch8/stack/stack.go
+++ ch8/stack/stack.go
@@ -1,6 +1,6 @@
 package stack
 
-type Stack[T any] struct {
+type Stack[T comparable] struct {
 	vals []T
 }
 
@@ -16,6 +16,15 @@ func (s *Stack[T]) Pop() (T, bool) {
 	top := s.vals[len(s.vals)-1]
 	s.vals = s.vals[:len(s.vals)-1]
 	return top, true
+}
+
+func (s Stack[T]) Contains(val T) bool {
+	for _, v := range s.vals {
+		if v == val {
+			return true
+		}
+	}
+	return false
 }
 
 func (s Stack[T]) Size() int {
