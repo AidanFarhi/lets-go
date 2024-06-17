46041232ee23691c38ac3cdc881a74fee53e2489
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-06-16 23:12:08 -0400

    updated ex1

--- ch13/ex1/main.go
+++ ch13/ex1/main.go
@@ -7,7 +7,12 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /time", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			w.Write([]byte("only GET allowed"))
+			return
+		}
 		w.Write([]byte(time.Now().Format(time.RFC3339)))
 	})
 	http.ListenAndServe(":8080", mux)
