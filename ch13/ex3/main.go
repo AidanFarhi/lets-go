92199e9261d2e0bac2e8cc4294fa68c1bd3bff7f
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-06-21 23:21:45 -0400

    added more to ch13

---
+++ ch13/ex3/main.go
@@ -0,0 +1,21 @@
+package main
+
+/*
+Add the ability to return the time as JSON. Use the Accept header to control
+whether JSON or text is returned (default to text).
+The JSON should be structured as follows:
+
+{
+    "day_of_week": "Monday",
+    "day_of_month": 10,
+    "month": "April",
+    "year": 2023,
+    "hour": 20,
+    "minute": 15,
+    "second": 20
+}
+*/
+
+func main() {
+
+}
