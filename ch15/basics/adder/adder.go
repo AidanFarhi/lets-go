ac9d546c53ec3eea121faaa6da698b996a2c422b
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-06-26 23:35:26 -0400

    added basics

---
+++ ch15/basics/adder/adder.go
@@ -0,0 +1,5 @@
+package adder
+
+func AddNums(x int, y int) int {
+	return x + y
+}
---
+++ ch15/basics/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"basics/adder"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(adder.AddNums(2, 3))
+}
