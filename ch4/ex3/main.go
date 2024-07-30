bc0e6c5ed0dc39c7325b305120b7026a842cc472
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-07-30 06:47:20 -0400

    finished up exercises

--- ch4/ex3/main.go
+++ ch4/ex3/main.go
@@ -1,5 +1,8 @@
-/*
+package main
 
+import "fmt"
+
+/*
 Start a new program. In main, declare an int variable called total.
 Write a for loop that uses a variable named i to iterate from 0 (inclusive) to 10 (exclusive).
 
@@ -10,5 +13,13 @@ fmt.Println(total)
 
 After the for loop, print out the value of total.
 What is printed out? What is the likely bug in this code?
-
 */
+
+func main() {
+	total := 0
+	for i := 0; i < 10; i++ {
+		total := total + i // this is shadowing the outer total variable. therefore, it is not being updated
+		fmt.Println(total)
+	}
+	fmt.Println(total)
+}
