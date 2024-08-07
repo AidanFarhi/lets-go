74903f6960b9c8b9e721210a6e72b425569cadfe
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-08-07 07:28:38 -0400

    added more to example code

--- ch7/examples/main.go
+++ ch7/examples/main.go
@@ -58,7 +58,6 @@ func (a Adder) AddTo(val int) int {
 
 // you can declare types based on other types
 type HighScore Score
-type Employee Person
 
 // iotas
 type MailCategory int
@@ -70,6 +69,64 @@ const (
 	Social
 	Advertisements
 )
+
+// use embedding for composition
+type Employee struct {
+	Name string
+	ID   string
+}
+
+func (e Employee) Description() string {
+	return fmt.Sprintf("%s (%s)", e.Name, e.ID)
+}
+
+// all fields or methods declared on an
+// embedded field are promoted to the containing struct
+type Manager struct {
+	Employee // this is an embedded field
+	Reports  []Employee
+}
+
+func (m Manager) DisplayReports() {
+	for _, e := range m.Reports {
+		fmt.Println(e.Description())
+	}
+}
+
+// if a containing struct has fields or methods with
+// the same name as the embedded field, you need to
+// use the embedded field's type to refer to the
+// obscured fields or methods.
+type Inner struct {
+	X int
+}
+
+type Outer struct {
+	Inner
+	X int
+}
+
+// there is no dynamic dispatch
+type InnerA struct {
+	A int
+}
+
+func (i InnerA) IntPrinter(val int) string {
+	return fmt.Sprintf("Inner: %d", val)
+}
+
+func (i InnerA) Double() string {
+	return i.IntPrinter(i.A * 2)
+}
+
+type OuterA struct {
+	InnerA
+	S string
+}
+
+func (o OuterA) IntPrinter(val int) string {
+	return fmt.Sprintf("Outer: %d", val)
+}
 
 func main() {
 
@@ -117,4 +174,34 @@ func main() {
 	var newScore Score = 50
 	scoreWithBonus := newScore + 100 // type of scoreWithBonus is Score
 	fmt.Println(scoreWithBonus)
+
+	manager := Manager{
+		Employee: Employee{
+			Name: "Bob Bobson",
+			ID:   "12345",
+		},
+		Reports: []Employee{},
+	}
+	fmt.Println(manager.ID)
+	fmt.Println(manager.Description())
+	manager.Reports = append(manager.Reports, Employee{"Jim Jimson", "44432"})
+	manager.Reports = append(manager.Reports, Employee{"Steve Jobs", "85854"})
+	manager.DisplayReports()
+
+	o := Outer{
+		Inner: Inner{
+			X: 100,
+		},
+		X: 200,
+	}
+	fmt.Println(o.X)
+	fmt.Println(o.Inner.X)
+
+	o2 := OuterA{
+		InnerA: InnerA{
+			A: 10,
+		},
+		S: "hello",
+	}
+	fmt.Println(o2.Double())
 }
