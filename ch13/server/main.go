25c53d61c2b8e51d96f5cd3894cc8292e18ab7ac
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-10-04 07:14:58 -0400

    added more example server code

--- ch13/server/main.go
+++ ch13/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
@@ -11,13 +12,46 @@ func (h HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello\n"))
 }
 
+func handleGoodbye(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("goodbye\n"))
+}
+
+func handleGreeting(w http.ResponseWriter, r *http.Request) {
+	name := r.PathValue("name")
+	w.Write([]byte(fmt.Sprintf("hello %s\n", name)))
+}
+
+func handleCatGreeting(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("meow!\n"))
+}
+
+func handleDogGreeting(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("bork!\n"))
+}
+
 func main() {
+
+	catMux := http.NewServeMux()
+	catMux.HandleFunc("GET /hello", handleCatGreeting)
+
+	dogMux := http.NewServeMux()
+	dogMux.HandleFunc("GET /hello", handleDogGreeting)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /goodbye", handleGoodbye)
+	mux.HandleFunc("GET /hello/{name}", handleGreeting)
+	mux.Handle("GET /hello", HelloHandler{})
+	mux.Handle("/cat/", http.StripPrefix("/cat", dogMux))
+	mux.Handle("/dog/", http.StripPrefix("/dog", dogMux))
+
 	s := http.Server{
 		Addr:         ":8080",
 		ReadTimeout:  30 * time.Second,
 		WriteTimeout: 90 * time.Second,
 		IdleTimeout:  120 * time.Second,
-		Handler:      HelloHandler{},
+		Handler:      mux,
 	}
+
 	s.ListenAndServe()
 }
