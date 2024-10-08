c71232d16f0b6ed01e17d442dede83c48e6ad1e2
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-10-08 08:39:11 -0400

    added tabletests

---
+++ ch15/tabletests/go.mod
@@ -0,0 +1,3 @@
+module tabletests
+
+go 1.22.0
---
+++ ch15/tabletests/maths_test.go
@@ -0,0 +1,35 @@
+package maths
+
+import "testing"
+
+func TestDoMath(t *testing.T) {
+	data := []struct {
+		name     string
+		x        int
+		y        int
+		op       string
+		expected int
+		errMsg   string
+	}{
+		{"addition", 2, 2, "+", 4, ""},
+		{"subtraction", 2, 2, "-", 0, ""},
+		{"multiplication", 2, 2, "*", 4, ""},
+		{"division", 2, 2, "/", 1, ""},
+		{"bad_division", 2, 0, "/", 0, `division by zero`},
+	}
+	for _, d := range data {
+		t.Run(d.name, func(t *testing.T) {
+			actual, err := DoMath(d.x, d.y, d.op)
+			if d.expected != actual {
+				t.Errorf("expected: %d actual: %d", d.expected, actual)
+			}
+			var errMsg string
+			if err != nil {
+				errMsg = err.Error()
+			}
+			if errMsg != d.errMsg {
+				t.Errorf("expected: `%s` actual: `%s`", d.errMsg, errMsg)
+			}
+		})
+	}
+}
